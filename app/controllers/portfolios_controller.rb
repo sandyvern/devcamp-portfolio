939e147da23af740f90a02977b34777aae4f0077
Sandy Hogan <SVern99@aol.com>
2017-02-14 22:34:52 +0000

    Implemented edit and update functionality, added form partial for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,29 +1,46 @@
 class PortfoliosController < ApplicationController
+  # before_action :set_portfolio_item_params, only: [:show, :edit, :update, :destroy]
   
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
   end
-  
+
   def create
-    @portfolio = portfolio.new(portfolio_item_params)
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
-      if @portfolio.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio item is now live.' }
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
   end
   
   private
   
-  def portfolion_item_params
-      params.require(:portfolio_item).permit(:title, :subtitle, :body, :main_image, :thumb_image)
-  end
+  # def set_portfolio_item_params
+  #   params.require(:portfolio_item).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+  # end
   
 end
