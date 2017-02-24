1053ca1fa4164a99dd967f56875da2e82649cc80
Sandy Hogan <SVern99@aol.com>
2017-02-24 20:56:50 +0000

    Implemented concern for image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,17 +1,19 @@
 class Portfolio < ApplicationRecord
-   validates_presence_of :title, :body, :main_image, :thumb_image
+  include Placeholder
+  
+  validates_presence_of :title, :body, :main_image, :thumb_image
    
-   # Two different ways of creating a custom scope
-   def self.angular
-     where(subtitle: "Angular")
-   end
+  # Two different ways of creating a custom scope
+  def self.angular
+    where(subtitle: "Angular")
+  end
    
-   scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
+  scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
+ 
+  after_initialize :set_defaults
    
-   after_initialize :set_defaults
-   
-   def set_defaults
-     self.main_image ||= "http://placehold.it/600x400" # Basically if main_image is nil then set it to this placeholder item
-     self.thumb_image ||= "http://placehold.it/350x200"
-   end
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400') # Basically if main_image is nil then set it to this placeholder item
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
 end
