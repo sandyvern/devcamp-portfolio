6490a67f0c7e28ebf449e2e185c52d6179cddc57
Sandy Hogan <SVern99@aol.com>
2017-03-09 22:42:48 +0000

    Added validations for topics in blogs and added a topic form element

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,7 +9,7 @@ class BlogsController < ApplicationController
     if logged_in?(:site_admin)
       @blogs = Blog.recent.page(params[:page]).per(5)
     else
-      @blogs = Blog.recent.published.page(params[:page]).per(5)
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
     end
     @page_title = "My Portfolio Blog"
   end
@@ -20,7 +20,7 @@ class BlogsController < ApplicationController
     if logged_in?(:site_admin) || @blog.published?
       @blog = Blog.includes(:comments).friendly.find(params[:id])
       @comment = Comment.new
-  
+
       @page_title = @blog.title
       @seo_keywords = @blog.body
     else
@@ -91,6 +91,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog).permit(:title, :body, :topic_id)
     end
 end
